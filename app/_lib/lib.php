07c4fc915810b1976fa39c876876046498ea2ae4
Volker Otto <pim.volker@gmail.com>
2014-07-13 01:50:47 +0200

    changed lib
    autoload vendors

---
+++ app/_lib/lib.php
@@ -0,0 +1,23 @@
+<?php
+/* ==========================================================================
+   AUTOLOAD - LIB
+   ========================================================================== */
+
+spl_autoload_register(function($className)){
+  $classFile = c::get('ROOT_LIB') . DS . $className . '.php';
+  if (file_exists($configFile)) require_once($configFile);
+}
+
+
+/* AUTOLOAD - Vendors
+   ========================================================================== */
+
+  // Load Slim
+  require_once (c::get('ROOT_VENDOR') . DS . 'slim' . DS . 'Slim.php');
+  \Slim\Slim::registerAutoloader();
+
+  // Load phpMailer
+  require_once (c::get('ROOT_VENDOR') . DS . 'phpmailer' . DS . 'PHPMailerAutoload.php');
+
+  // Load Mailchimp
+  require_once (c::get('ROOT_VENDOR') . DS . 'mailchimp-api' . DS . 'mailchimp.php');
