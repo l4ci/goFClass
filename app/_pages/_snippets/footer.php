ad6a385a79bd853e3ccaac23708239c9cb40e9bb
Volker Otto <pim.volker@gmail.com>
2014-08-02 22:55:46 +0200

    main page footer

---
+++ app/_pages/_snippets/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <ul>
+      <li><a href="#">AGB</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Impressum</a></li>
+    </ul>
+  </div><!-- .container -->
+</footer><!-- .footer -->
